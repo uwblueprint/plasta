49e2844bab8de2e0290fef518072731f5d391f25
Xin Hao Zhang <xinhao.zhang98@gmail.com>
2018-06-24 15:45:16 -0400

    Redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
