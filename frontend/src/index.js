81cb0e0af362ab3a845a96faded01fbf91ea8bf1
Xin Hao Zhang <xinhao.zhang98@gmail.com>
2018-07-01 01:43:14 -0400

    Use react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 import rootReducer from './reducers';
 
 const store = createStore(rootReducer);
 
 render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <div>
+        <Route path="/" component={App} />
+        {/* add routes here */}
+      </div>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
