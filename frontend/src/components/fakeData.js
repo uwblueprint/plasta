d126ca4f36f4016195226df93a77787ba0db7eb8
Nick Yang <nick.yang3721@gmail.com>
2018-07-01 22:27:36 -0400

    Added static TransactionHistory component

---
+++ frontend/src/components/fakeData.js
@@ -0,0 +1,28 @@
+const fakeData = [
+  {
+    to: 'Gautaum',
+    from: 'Xin Hao',
+    rs: 500,
+    kg: 30,
+    type: 'Green PET',
+    date: 'May 2018',
+  },
+  {
+    to: 'Clayton',
+    from: 'Nick',
+    rs: 1000,
+    kg: 123,
+    type: 'Brown PET',
+    date: 'February 2017',
+  },
+  {
+    to: 'Joslyn',
+    from: 'Ahmed',
+    rs: 5033,
+    kg: 123,
+    type: 'Mixed PET',
+    date: 'January 2019',
+  },
+];
+
+export { fakeData };
