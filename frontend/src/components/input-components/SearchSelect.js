928061ee339e598eeddb9a5596289bde7bde7282
Nick Yang <nick.yang3721@gmail.com>
2018-07-08 15:25:23 -0400

    :dancers: Added NewProject and a multi-select input for DWCCs

---
+++ frontend/src/components/input-components/SearchSelect.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+/*
+	render() {
+		return (
+			<MultiSelect
+				selectedOption = this.state.whatever
+				dataLabel="plasticType",
+				onChange=this.onChange,
+				options = ....
+			/>);
+	}*/
+
+class SearchSelect extends Component {
+  constructor(props) {
+    super(props);
+    if (!props.selectedOption) this.state = { selectedOption: '' };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(selectedOption) {
+    if (this.props.onChange) this.props.onChange(selectedOption);
+    else this.setState({ selectedOption });
+  }
+
+  render() {
+    const { options, multi } = this.props;
+    const selectedOption = this.props.selectedOption || this.state.selectedOption;
+    return (
+      <div className="search-select-wrapper">
+        {this.props.title && <h1> {this.props.title} </h1>}
+        <Select
+          name="form-field-name"
+          value={selectedOption}
+          onChange={this.onChange}
+          options={options}
+          multi={multi}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchSelect;
