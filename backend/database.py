75b270f0c45f95ddc023c2df040e4d214a70c98a
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2018-07-15 17:13:42 -0400

    Integrated flask into our database creation Python script.

--- backend/database.py
+++ backend/database.py
@@ -1,51 +1,49 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, Text, String, Boolean
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Replace 'sqlite:///rfg.db' with your path to database
-engine = create_engine('sqlite:///test.db', convert_unicode=True)
-db_session = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-class Users(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    stakeholder_id = Column(Integer, nullable=False)
-    phone_number = Column(String(15))
-    email = Column(String(254))
-    notes = Column(Text)
-    password = Column(Text, nullable=False)
-    forgot_password_token = Column(String(10))
-    created = Column(DateTime, nullable=False)
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    stakeholder_id = db.Column(db.Integer, nullable=False)
+    phone_number = db.Column(db.String(15))
+    email = db.Column(db.String(254))
+    notes = db.Column(db.Text)
+    password = db.Column(db.Text, nullable=False)
+    forgot_password_token = db.Column(db.String(10))
+    created = db.Column(db.DateTime, nullable=False)
 
 
-class UserAttributes(Base):
-    __tablename__ = 'user_attributes'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text, nullable=False)
-    data_type = Column(Text, nullable=False)
-    is_required = Column(Boolean, nullable=False)
+class UserAttribute(db.Model):
+    __tablename__ = 'user_attribute'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    data_type = db.Column(db.Text, nullable=False)
+    is_required = db.Column(db.Boolean, nullable=False)
 
 
-class UserValuesInt(Base):
-    __tablename__ = 'user_values_int'
-    id = Column(Integer, primary_key=True)
-    attribute_id = Column(Integer, ForeignKey('user_attribute.id'))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    value = Column(Integer, nullable=False)
+class UserValueInt(db.Model):
+    __tablename__ = 'user_value_int'
+    id = db.Column(db.Integer, primary_key=True)
+    attribute_id = db.Column(db.Integer, db.ForeignKey('user_attribute.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    value = db.Column(db.Integer, nullable=False)
 
 
-class UserValuesText(Base):
-    __tablename__ = 'user_values_text'
-    id = Column(Integer, primary_key=True)
-    attribute_id = Column(Integer, ForeignKey('user_attribute.id'))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    value = Column(Text, nullable=False)
+class UserValueText(db.Model):
+    __tablename__ = 'user_value_text'
+    id = db.Column(db.Integer, primary_key=True)
+    attribute_id = db.Column(db.Integer, db.ForeignKey('user_attribute.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    value = db.Column(db.Text, nullable=False)
 
 
-Base.metadata.create_all(engine)
+db.create_all()
