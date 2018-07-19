c864a7f90462108ea9d99817f437aa48be72789c
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2018-07-19 19:23:56 -0400

    Updated database schema to use Flask and Flask Migrate.

--- backend/database.py
+++ backend/database.py
@@ -1,147 +1,154 @@
 from datetime import datetime
-from sqlalchemy import create_engine, Table
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, scoped_session, sessionmaker
-from sqlalchemy.sql import func
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, Text, String, Boolean, Float
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-engine = create_engine('sqlite:///test.db', convert_unicode=True)
-db_session = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Many to many relationship
-projects_association_table = Table(
-    'projects_association', Base.metadata,
-    Column('project_id', Integer, ForeignKey('projects.id'), nullable=False),
-    Column('user_id', Integer, ForeignKey('users.id'), nullable=False))
+projects_association_table = db.Table(
+    'projects_association', db.Model.metadata,
+    db.Column(
+        'project_id', db.Integer, db.ForeignKey('projects.id'),
+        nullable=False),
+    db.Column(
+        'user_id', db.Integer, db.ForeignKey('users.id'), nullable=False))
 
 
-class Users(Base):
+class Users(db.Model):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    stakeholder_id = Column(Integer, nullable=False)
-    phone_number = Column(String(15))
-    email = Column(String(254))
-    notes = Column(Text)
-    password = Column(Text, nullable=False)
-    forgot_password_token = Column(String(10))
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now())
-    projects = relationship('Projects', secondary=projects_association_table)
+    id = db.Column(db.Integer, primary_key=True)
+    stakeholder_id = db.Column(db.Integer, nullable=False)
+    phone_number = db.Column(db.String(15))
+    email = db.Column(db.String(254))
+    notes = db.Column(db.Text)
+    password = db.Column(db.Text, nullable=False)
+    forgot_password_token = db.Column(db.String(10))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    projects = db.relationship(
+        'Projects', secondary=projects_association_table)
 
 
-class UserAttributes(Base):
+class UserAttributes(db.Model):
     __tablename__ = 'user_attributes'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text, nullable=False)
-    data_type = Column(Text, nullable=False)
-    is_required = Column(Boolean, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    data_type = db.Column(db.Text, nullable=False)
+    is_required = db.Column(db.Boolean, nullable=False)
 
 
-class UserValuesInt(Base):
+class UserValuesInt(db.Model):
     __tablename__ = 'user_values_int'
-    id = Column(Integer, primary_key=True)
-    attribute_id = Column(
-        Integer, ForeignKey('user_attributes.id'), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    value = Column(Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    attribute_id = db.Column(
+        db.Integer, db.ForeignKey('user_attributes.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    value = db.Column(db.Integer, nullable=False)
 
 
-class UserValuesText(Base):
+class UserValuesText(db.Model):
     __tablename__ = 'user_values_text'
-    id = Column(Integer, primary_key=True)
-    attribute_id = Column(
-        Integer, ForeignKey('user_attributes.id'), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    value = Column(Text, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    attribute_id = db.Column(
+        db.Integer, db.ForeignKey('user_attributes.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    value = db.Column(db.Text, nullable=False)
 
 
-class Projects(Base):
+class Projects(db.Model):
     __tablename__ = 'projects'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    description = Column(Text, nullable=False)
-    type = Column(Integer, nullable=False)
-    gdrive_link = Column(Integer, nullable=False)
-    shipping_terms = Column(Integer, nullable=False)
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now())
-    users = relationship('Users', secondary=projects_association_table)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    type = db.Column(db.Integer, nullable=False)
+    gdrive_link = db.Column(db.Integer, nullable=False)
+    shipping_terms = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    users = db.relationship('Users', secondary=projects_association_table)
 
 
-class CostModel(Base):
+class CostModel(db.Model):
     __tablename__ = 'cost_model'
-    project_id = Column(Integer, ForeignKey('projects.id'), primary_key=True)
-    wastepicker_sell_price = Column(Float, nullable=False)
-    wholesaler_purchase_price = Column(Float, nullable=False)
-    wholesaler_sell_price = Column(Float, nullable=False)
-    price_buoyancy = Column(Float, nullable=False)
-    wholesaler_exworks_price = Column(Float, nullable=False)
-    wholesaler_shipping_price = Column(Float, nullable=False)
-    wholesaler_delivered_price = Column(Float, nullable=False)
-    pfc_transaction_fee = Column(Float, nullable=False)
+    project_id = db.Column(
+        db.Integer, db.ForeignKey('projects.id'), primary_key=True)
+    wastepicker_sell_price = db.Column(db.Float, nullable=False)
+    wholesaler_purchase_price = db.Column(db.Float, nullable=False)
+    wholesaler_sell_price = db.Column(db.Float, nullable=False)
+    price_buoyancy = db.Column(db.Float, nullable=False)
+    wholesaler_exworks_price = db.Column(db.Float, nullable=False)
+    wholesaler_shipping_price = db.Column(db.Float, nullable=False)
+    wholesaler_delivered_price = db.Column(db.Float, nullable=False)
+    pfc_transaction_fee = db.Column(db.Float, nullable=False)
 
 
-class Transactions(Base):
+class Transactions(db.Model):
     __tablename__ = 'transactions'
-    id = Column(Integer, primary_key=True)
-    project_id = Column(Integer, ForeignKey('projects.id'), nullable=False)
-    from_user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    to_user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    price = Column(Float, nullable=False)
-    image = Column(String(255), nullable=False)
-    sale_date = Column(DateTime, default=datetime.utcnow)
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now())
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(
+        db.Integer, db.ForeignKey('projects.id'), nullable=False)
+    from_user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    to_user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    sale_date = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
 
-class TransactionAttributes(Base):
+class TransactionAttributes(db.Model):
     __tablename__ = 'transaction_attributes'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text, nullable=False)
-    data_type = Column(Text, nullable=False)
-    is_required = Column(Boolean, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    data_type = db.Column(db.Text, nullable=False)
+    is_required = db.Column(db.Boolean, nullable=False)
 
 
-class TransactionValuesFloat(Base):
+class TransactionValuesFloat(db.Model):
     __tablename__ = 'transaction_values_float'
-    id = Column(Integer, primary_key=True)
-    attribute_id = Column(
-        Integer, ForeignKey('transaction_attributes.id'), nullable=False)
-    transaction_id = Column(
-        Integer, ForeignKey('transactions.id'), nullable=False)
-    value = Column(Float, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    attribute_id = db.Column(
+        db.Integer, db.ForeignKey('transaction_attributes.id'), nullable=False)
+    transaction_id = db.Column(
+        db.Integer, db.ForeignKey('transactions.id'), nullable=False)
+    value = db.Column(db.Float, nullable=False)
 
 
-class TransactionValuesInt(Base):
+class TransactionValuesInt(db.Model):
     __tablename__ = 'transaction_values_int'
-    id = Column(Integer, primary_key=True)
-    attribute_id = Column(
-        Integer, ForeignKey('transaction_attributes.id'), nullable=False)
-    transaction_id = Column(
-        Integer, ForeignKey('transactions.id'), nullable=False)
-    value = Column(Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    attribute_id = db.Column(
+        db.Integer, db.ForeignKey('transaction_attributes.id'), nullable=False)
+    transaction_id = db.Column(
+        db.Integer, db.ForeignKey('transactions.id'), nullable=False)
+    value = db.Column(db.Integer, nullable=False)
 
 
-class TransactionValuesText(Base):
+class TransactionValuesText(db.Model):
     __tablename__ = 'transaction_values_text'
-    id = Column(Integer, primary_key=True)
-    attribute_id = Column(
-        Integer, ForeignKey('transaction_attributes.id'), nullable=False)
-    transaction_id = Column(
-        Integer, ForeignKey('transactions.id'), nullable=False)
-    value = Column(Text, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    attribute_id = db.Column(
+        db.Integer, db.ForeignKey('transaction_attributes.id'), nullable=False)
+    transaction_id = db.Column(
+        db.Integer, db.ForeignKey('transactions.id'), nullable=False)
+    value = db.Column(db.Text, nullable=False)
 
 
-class TransactionParents(Base):
+class TransactionParents(db.Model):
     __tablename__ = 'transaction_parents'
-    transaction_id = Column(
-        Integer, ForeignKey('transactions.id'), primary_key=True)
-    parent_id = Column(
-        Integer, ForeignKey('transactions.id'), primary_key=True)
+    transaction_id = db.Column(
+        db.Integer, db.ForeignKey('transactions.id'), primary_key=True)
+    parent_id = db.Column(
+        db.Integer, db.ForeignKey('transactions.id'), primary_key=True)
 
 
 if __name__ == '__main__':
-    Base.metadata.create_all(engine)
+    db.create_all()
