e55f064364b77fefcc4ce4c73893ebdbcfa8b10f
Clayton Halim <claytonhalim3@gmail.com>
2018-08-26 01:35:14 -0400

    Separate add function with helpers
    Add doc strings to functions and module

--- backend/tools/seed.py
+++ backend/tools/seed.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-
-import sys
-sys.path.append('..')
+"""
+A command line tool to create data on the database
+"""
 
 from pprint import pprint
 import click
 import bcrypt
-from app.app import db
-from app.models import User, Vendor, vendor_type_enum
+from ..app.app import db
+from ..app.models import User, Vendor, vendor_type_enum
 
-table_choice = ('user', 'vendor')
+TABLE_CHOICE = ('user', 'vendor')
 
 
 @click.group()
@@ -17,39 +17,50 @@ def database():
     pass
 
 
-@database.command()
-@click.option('--table', type=click.Choice(table_choice), required=True)
-def add(table):
-    params = {}
-    if table == 'user':
-        params = {
-            'vendor_id':
-            click.prompt('Vendor ID', type=int),
-            'email':
-            click.prompt('Email'),
-            'active':
-            click.prompt('Active [y/N]', type=bool),
-            'password_hash':
-            bcrypt.hashpw(
-                click.prompt(
-                    'Password', hide_input=True, confirmation_prompt=True)
-                .encode('utf8'), bcrypt.gensalt(14))
-        }
-        params = {
-            param: value
-            for param, value in params.items() if value != ''
-        }
-        new_object = User(**params)
-    elif table == 'vendor':
-        params = {
-            'vendor_type':
+def _add_user():
+    """Helper function for creating a user in the database"""
+
+    params = {
+        'vendor_id':
+        click.prompt('Vendor ID', type=int),
+        'email':
+        click.prompt('Email'),
+        'active':
+        click.prompt('Active [y/N]', type=bool),
+        'password_hash':
+        bcrypt.hashpw(
             click.prompt(
-                'Vendor Type',
-                type=click.Choice(vendor_type_enum.enums)),
-            'name':
-            click.prompt('Name')
-        }
-        new_object = Vendor(**params)
+                'Password', hide_input=True, confirmation_prompt=True)
+            .encode('utf8'), bcrypt.gensalt(14))
+    }
+    params = {param: value for param, value in params.items() if value != ''}
+    return User(**params)
+
+
+def _add_vendor():
+    """Helper function for creating a vendor in the database"""
+
+    params = {
+        'vendor_type':
+        click.prompt('Vendor Type', type=click.Choice(vendor_type_enum.enums)),
+        'name':
+        click.prompt('Name')
+    }
+    return Vendor(**params)
+
+
+@database.command()
+@click.option('--table', type=click.Choice(TABLE_CHOICE), required=True)
+def add(table):
+    """Creates a new object in the database
+    
+    Parameters:
+    table   Name of table to add object to
+    """
+
+    choices = {'user': _add_user, 'vendor': _add_vendor}
+
+    new_object = choices[table]()
     db.session.add(new_object)
     db.session.flush()
     print('Sucessfully created')
@@ -58,9 +69,15 @@ def add(table):
 
 
 @database.command()
-@click.option('--table', type=click.Choice(table_choice), required=True)
+@click.option('--table', type=click.Choice(TABLE_CHOICE), required=True)
 @click.option('--object_id', type=int, required=True)
 def delete(table, object_id):
+    """Deletes a object in the database
+    Parameters:
+    object_id   ID of object to delete
+    table       Name of table to delete object from
+    """
+
     table_types = {'user': User, 'vendor': Vendor}
     table = table_types[table]
     result = table.query.get(object_id)
