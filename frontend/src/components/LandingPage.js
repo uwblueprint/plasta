f58420198874861b977f0f6c4ffd1c6205fb2d76
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2018-09-05 19:26:00 -0400

    Changed landing page routing to only have one landing page and load components based on a state variable. Using React Fragments to organize code more efficiently.

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './LandingPage.css';
 
 class LandingPage extends Component {
+  state = {
+    is_admin: true,
+  };
+
+  load_admin_page = (
+    <Fragment>
+      <button className="btn" type="button" onClick={() => this.props.history.push('admindash')}>
+        Projects
+      </button>
+      <button
+        className="btn"
+        type="button"
+        onClick={() => this.props.history.push('/admin/stakeholders')}
+      >
+        Stakeholders
+      </button>
+    </Fragment>
+  );
+
+  load_dwcc_page = (
+    <Fragment>
+      <button className="btn" type="button">
+        Most Recent Project
+      </button>
+      <button className="btn" type="button">
+        Projects
+      </button>
+      <button className="btn" type="button">
+        Wastepickers
+      </button>
+    </Fragment>
+  );
+
   render() {
     return (
       <div className="page-wrapper vcentre-outer" id="landing-wrapper">
         <div className="vcentre">
-          <button
-            className="btn"
-            type="button"
-            onClick={() => this.props.history.push('admindash')}
-          >
-            Projects
-          </button>
-          <button
-            className="btn"
-            type="button"
-            onClick={() => this.props.history.push('/admin/stakeholders')}
-          >
-            Stakeholders
-          </button>
+          {this.state.is_admin ? this.load_admin_page : this.load_dwcc_page}
         </div>
       </div>
     );
