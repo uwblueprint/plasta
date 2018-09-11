8ff68425d4c40d4af90fe812886b0c74e033d6e6
Clayton Halim <claytonhalim3@gmail.com>
2018-09-11 19:45:20 -0400

    Refactor flask app to support blueprints

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,2 +1,19 @@
-__all__ = ['app']
-from . import models
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+migrate.init_app(app, db)
+
+#pylint: disable=wrong-import-position
+from app.project.routes import project
+
+app.register_blueprint(project)
