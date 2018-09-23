4d96c9620a8f8483cb062c833f9c33f441803ba0
imruahmed <im.ahmed97@gmail.com>
2018-09-23 12:51:37 -0400

    Restructure backend architecture

--- backend/app/routes/project_routes.py
+++ backend/app/routes/project_routes.py
@@ -1,11 +1,10 @@
 from flask import Blueprint, jsonify, request
-from app import db
-from app.models import Project
+from app import db_client
 
-project = Blueprint('project', __name__)
+project_blueprint = Blueprint('project', __name__)
 
 
-@project.route('/project', methods=['POST'])
+@project_blueprint.route('/project', methods=['POST'])
 def handle_project():
     if request.method == 'POST':
         return _make_project()
@@ -14,7 +13,5 @@ def handle_project():
 
 def _make_project():
     data = request.json
-    new_project = Project(**data)
-    db.session.add(new_project)
-    db.session.commit()
+    db_client.save_project(data)
     return jsonify(success=True)
