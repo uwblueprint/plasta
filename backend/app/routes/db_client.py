9f34c791e466c46914dfb216ec6c3a7433775fe1
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2018-09-30 21:30:09 -0400

    Add get all projects endpoint

--- backend/app/routes/db_client.py
+++ backend/app/routes/db_client.py
@@ -1,8 +1,12 @@
 from ..models.models import Project, User, Transaction
 
 
-def save_project(formatted_data):
-    return Project.create(**formatted_data)
+def save_project(data):
+    return Project.create(**data)
+
+
+def get_projects():
+    return Project.query.all()
 
 
 def get_project_transactions(project_id):
--- backend/app/routes/project_routes.py
+++ backend/app/routes/project_routes.py
@@ -1,28 +1,34 @@
-from flask import Blueprint, request
-
-from .route_utils import success
 from . import db_client
+from .route_utils import success, error_response
+from .formatters import project_formatter
+from flask import Blueprint, request
 
 project_blueprint = Blueprint('project', __name__)
 
 
-@project_blueprint.route('/project', methods=['GET', 'POST'])
+@project_blueprint.route('/projects', methods=['GET', 'POST'])
 def handle_project():
     if request.method == 'GET':
-        return success(message='Successfully passed a JWT')
+        return _get_projects()
     if request.method == 'POST':
         return _make_project()
-    return None
+    return error_response('Invalid Request')
+
+
+def _get_projects():
+    projects = db_client.get_projects()
+    formatted_projects = project_formatter.format_projects(projects)
+    return success(data=list(formatted_projects))
 
 
 def _make_project():
-    data = request.json
-    db_client.save_project(data)
+    project_data = request.json
+    db_client.save_project(project_data)
     return success(message='Successfully passed a JWT')
 
 
 @project_blueprint.route(
-    '/project/<int:project_id>/transactions', methods=['GET'])
+    '/projects/<int:project_id>/transactions', methods=['GET'])
 def get_project_transactions(project_id):
     transactions = db_client.get_project_transactions(project_id)
     return success(
