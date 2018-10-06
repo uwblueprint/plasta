1c6fa7f386ee12f9fa8f46aeab7d496fdb47cdca
imruahmed <im.ahmed97@gmail.com>
2018-10-06 19:17:14 -0400

    Add CRUDMixin + txn route

---
+++ backend/app/models/mixins.py
@@ -0,0 +1,48 @@
+from . import db
+
+
+# Stolen from https://gist.github.com/YukSeungChan/851967b04d1f7cb6ba56
+
+
+# This Mixin object allows us to implement a CRUD interface with all of our
+# model objects. We get a clean way to get, save, update and delete our objects.
+class CRUDMixin(object):
+    __table_args__ = {'extend_existing': True}
+
+    @classmethod
+    def get(cls, _id):
+        return cls.query.get(int(_id))
+
+    @classmethod
+    def get_by(cls, first=False, **kwargs):
+        rows = cls.query.filter_by(**kwargs)
+        if first:
+            return rows.first()
+        return rows.all()
+
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        db.session.add(self)
+        if commit:
+            try:
+                db.session.commit()
+            except Exception:
+                db.session.rollback()
+                raise
+        return self
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        return commit and db.session.commit()
+
+    def to_dict(self):
+        return self._as_dict()
