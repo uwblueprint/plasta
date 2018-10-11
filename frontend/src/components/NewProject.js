eb05aab92e80ec4311af1ce4a2d04ff0d40e530f
mmqw <mary.mqwang@gmail.com>
2018-10-11 19:50:44 -0400

    Updating fields based on Tammy's feedback
    Adding metadata fields to new project

--- frontend/src/components/NewProject.js
+++ frontend/src/components/NewProject.js
@@ -11,6 +11,23 @@ import './NewProject.css';
 
 const staticDWCC = [{ label: 'DWCC 1', value: 'dw1' }, { label: 'DWCC 2', value: 'dw2' }];
 
+const staticWholesaler = [
+  { label: 'Wholesaler 1', value: 'WS1' },
+  { label: 'Wholesaler 2', value: 'WS2' },
+];
+
+const staticBrands = [
+  { label: 'Brand A', value: 'a' },
+  { label: 'Brand B', value: 'b' },
+  { label: 'Brand C', value: 'c' },
+];
+
+const staticShippingTerms = [
+  { label: 'FOB', value: 'fob' },
+  { label: 'CIF', value: 'cif' },
+  { label: 'DDP', value: 'ddp' },
+];
+
 class NewProject extends Component {
   constructor(props) {
     super(props);
@@ -19,19 +36,20 @@ class NewProject extends Component {
       brandName: '',
       description: '',
       gDriveLink: '',
-      projectStatus: 'archive',
       projectType: 'external',
       dwccSelected: [],
       wholesalerSelected: [],
-      wastepickerSell: '',
+      wholesalerSellPrice: '',
       wastepickerSellPrice: '',
-      wholesalerSell: '',
-      wholesalerPurchase: '',
-      transactionFee: '',
+      dwccSellPrice: '',
+      shippingAddress: '',
+      shippingTerms: '',
+      poNumber: '',
+      startDate: '',
+      endDate: '',
       priceBuoyancy: '',
-      wholesalerExworks: '',
-      deliveredAmount: '',
-      shippedAmount: '',
+      shippingPrice: '',
+      deliveredPrice: '',
       plasticQuantities: List(),
     };
     this.onFieldChange = this.onFieldChange.bind(this);
@@ -43,10 +61,39 @@ class NewProject extends Component {
   }
 
   onSubmit() {
+    const url = process.env.REACT_APP_API_URL + 'project';
+    const plasticTypes = this.state.plasticQuantities
+      .map(pair => pair.get('plasticType'))
+      .toArray();
+
+    const metaData = {
+      brand_name: this.state.brandName,
+      description: this.state.description,
+      gdrive_link: this.state.gDriveLink,
+      project_type: this.state.projectType,
+      project_status: this.state.projectStatus,
+      dwcc_selected: this.state.dwccSelected,
+      wholesaler_selected: this.state.wholesalerSelected,
+      shipping_address: this.state.shippingAddress,
+      shipping_terms: this.state.shippingTerms,
+      po_number: this.state.poNumber,
+      start_date: this.state.startDate,
+      end_date: this.state.endDate,
+      cost_model: {
+        wholesaler_sell_price: this.state.wholesalerSellPrice,
+        wastepicker_sell_price: this.state.wastepickerSellPrice,
+        dwcc_sell_price: this.state.dwccSellPrice,
+        price_buoyancy: this.state.priceBuoyancy,
+        wholesaler_shipping_price: this.state.shippingPrice,
+        // sell price + shipping_price = delivered_price
+        wholesaler_delivered_price: this.state.deliveredPrice,
+      },
+    };
     const newProjectData = {
       name: this.state.projectName,
       project_type: this.state.projectType,
-      plastics: this.state.plasticQuantities,
+      plastics: plasticTypes,
+      meta_data: metaData,
     };
     // TODO: (XIN) Handle proj creation error
     post('/projects', newProjectData).catch(err => {});
@@ -96,15 +143,6 @@ class NewProject extends Component {
           />
         </FormSection>
 
-        <FormSection title="Status">
-          <RadioSelect
-            field="projectStatus"
-            selectedValue={this.state.projectStatus}
-            options={[{ label: 'Archive', value: 'archive' }, { label: 'Active', value: 'active' }]}
-            onChange={this.onFieldChange}
-          />
-        </FormSection>
-
         <FormSection title="Plastic Types">
           <PlasticTypeQuantityGroup
             label="List of Plastic Types"
@@ -118,11 +156,7 @@ class NewProject extends Component {
           <SearchSelect
             field="brandName"
             selectedOption={this.state.brandName}
-            options={[
-              { label: 'Brand A', value: 'a' },
-              { label: 'Brand B', value: 'b' },
-              { label: 'Brand C', value: 'c' },
-            ]}
+            options={staticBrands}
             onChange={this.onFieldChange}
           />
         </FormSection>
@@ -137,56 +171,88 @@ class NewProject extends Component {
           />
         </FormSection>
 
-        <FormSection title="Wastepicker">
-          <TextInput
-            leftlabel="Sell"
-            rightlabel="Rs/Kg"
-            type="number"
-            field="wastepickerSell"
-            value={this.state.wastepickerSell}
-            onChange={this.onFieldChange}
-          />
-          <TextInput
-            leftlabel="Sell Price"
-            rightlabel="₹"
-            type="number"
-            field="wastepickerSellPrice"
-            value={this.state.wastepickerSellPrice}
-            onChange={this.onFieldChange}
-          />
-        </FormSection>
-
         <FormSection title="Wholesaler">
-          <TextInput
-            leftlabel="Sell"
-            rightlabel="Rs/Kg"
-            type="number"
-            field="wholesalerSell"
-            value={this.state.wholesalerSell}
+          <SearchSelect
+            options={staticWholesaler}
+            field="wholesalerSelected"
             onChange={this.onFieldChange}
+            selectedOption={this.state.wholesalerSelected}
+            multi
           />
+        </FormSection>
+
+        <FormSection title="Shipping Address">
           <TextInput
-            leftlabel="Purchase"
-            type="number"
-            rightlabel="Rs/Kg"
-            field="wholesalerPurchase"
-            value={this.state.wholesalerPurchase}
+            field="shippingAddress"
+            className="full-width"
+            value={this.state.shippingAddress}
             onChange={this.onFieldChange}
           />
         </FormSection>
 
-        <FormSection title="PFC">
+        <FormSection title="Shipping Terms">
+          <SearchSelect
+            field="shippingTerms"
+            selectedOption={this.state.shippingTerms}
+            options={staticShippingTerms}
+            onChange={this.onFieldChange}
+          />
+        </FormSection>
+
+        <FormSection title="PO Number">
           <TextInput
-            leftlabel="Transaction Fee"
-            rightlabel="₹"
-            type="number"
-            field="transactionFee"
-            value={this.state.transactionFee}
+            className="full-width"
+            field="poNumber"
+            value={this.state.poNumber}
+            onChange={this.onFieldChange}
+          />
+        </FormSection>
+
+        <FormSection title="Start Date">
+          <TextInput
+            className="full-width"
+            field="startDate"
+            value={this.state.startDate}
+            placeholder="YYYY/MM/DD"
+            onChange={this.onFieldChange}
+          />
+        </FormSection>
+
+        <FormSection title="End Date">
+          <TextInput
+            className="full-width"
+            field="endDate"
+            value={this.state.endDate}
+            placeholder="YYYY/MM/DD"
             onChange={this.onFieldChange}
           />
         </FormSection>
 
         <FormSection title="Cost Model">
+          <TextInput
+            leftlabel="Wholesaler Sell Price"
+            rightlabel="₹/kg"
+            type="number"
+            field="wholesalerSellPrice"
+            value={this.state.wholesalerSellPrice}
+            onChange={this.onFieldChange}
+          />
+          <TextInput
+            leftlabel="Wastepicker Sell Price"
+            rightlabel="₹kg"
+            type="number"
+            field="wastepickerSellPrice"
+            value={this.state.wastepickerSellPrice}
+            onChange={this.onFieldChange}
+          />
+          <TextInput
+            leftlabel="DWCC Sell Price"
+            rightlabel="₹/kg"
+            type="number"
+            field="dwccSellPrice"
+            value={this.state.dwccSellPrice}
+            onChange={this.onFieldChange}
+          />
           <TextInput
             leftlabel="Price Buoyancy"
             rightlabel="₹"
@@ -196,27 +262,19 @@ class NewProject extends Component {
             onChange={this.onFieldChange}
           />
           <TextInput
-            leftlabel="Wholesaler Exworks"
-            rightlabel="₹"
-            type="number"
-            field="wholesalerExworks"
-            value={this.state.wholesalerExworks}
-            onChange={this.onFieldChange}
-          />
-          <TextInput
             leftlabel="Delivered"
             rightlabel="₹"
             type="number"
-            field="deliveredAmount"
-            value={this.state.deliveredAmount}
+            field="deliveredPrice"
+            value={this.state.deliveredPrice}
             onChange={this.onFieldChange}
           />
           <TextInput
             leftlabel="Shipped"
             rightlabel="₹"
             type="number"
-            field="shippedAmount"
-            value={this.state.shippedAmount}
+            field="shippedPrice"
+            value={this.state.shippedPrice}
             onChange={this.onFieldChange}
           />
         </FormSection>
