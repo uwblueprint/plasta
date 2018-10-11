eedd43c500dfb905b5f53fad15e025d6a672b7cb
Xin Hao Zhang <xinhao.zhang98@gmail.com>
2018-10-11 19:34:21 -0400

    Rename globalHelpers to request and shorten method name, remove unnecesary line

--- frontend/src/components/utils/requests.js
+++ frontend/src/components/utils/requests.js
@@ -1,5 +1,4 @@
-//
-export function postHTTPRequest(path, payload) {
+export function post(path, payload) {
   const url = process.env.REACT_APP_API_URL + path;
   const data = {
     method: 'POST',
@@ -10,7 +9,7 @@ export function postHTTPRequest(path, payload) {
   };
   return fetch(url, data)
     .then(response => {
-      if (response.ok) return response.json;
+      if (response.ok) return response.json();
       throw response;
     })
     .catch(err => console.error(err));
