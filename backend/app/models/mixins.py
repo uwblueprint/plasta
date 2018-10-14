60c5074fd3a806bc273ddd9f8d492cf01e825ad5
Clayton Halim <claytonhalim3@gmail.com>
2018-10-14 14:51:52 -0400

    Add metadata sanitization and insertion

--- backend/app/models/mixins.py
+++ backend/app/models/mixins.py
@@ -17,16 +17,43 @@ class BaseMixin(object):
             return rows.first()
         return rows.all()
 
+    @classmethod
+    def sanitize_metadata(cls, metadata, specification):
+        sanitized = {}
+
+        for field, rules in specification.items():
+            if not rules['required'] and field not in metadata:
+                continue
+
+            datum = metadata[field]
+            required_type = specification[field]['type']
+
+            if isinstance(datum, dict):
+                sanitized[field] = cls.sanitize_metadata(datum, rules['specification'])
+            elif isinstance(datum, specification[field]['type']):
+                sanitized[field] = datum
+            else:
+                raise TypeError(
+                    f'Metadata type, {type(datum)} does not match requirement, {required_type}'
+                )
+
+        return sanitized
+
     # TODO(imran): Find abstraction to recursively create subobjects
     @classmethod
     def create(cls, **kwargs):
+        if 'meta_data' in kwargs and cls.METADATA_SPECIFICATION is not None:
+            kwargs['meta_data'] = cls.sanitize_metadata(
+                kwargs['meta_data'], cls.METADATA_SPECIFICATION)
         instance = cls(**kwargs)
         return instance.save()
+
 
     def update(self, commit=True, **kwargs):
         for attr, value in kwargs.items():
             setattr(self, attr, value)
         return commit and self.save() or self
+
 
     def save(self, commit=True):
         db.session.add(self)
@@ -38,12 +65,15 @@ class BaseMixin(object):
                 raise
         return self
 
+
     def delete(self, commit=True):
         db.session.delete(self)
         return commit and db.session.commit()
 
+
     def to_dict(self):
         return vars(self)
+
 
     def format(self, include_relationships=False):
         cls = type(self)
--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -36,6 +36,63 @@ class Project(BaseMixin, db.Model):
     plastics = db.relationship('ProjectPlasticMap', back_populates='project')
     meta_data = db.Column(JSONB)
 
+    METADATA_WHITELIST = {
+        'brand_name': {
+            'type': str,
+            'required': True
+        },
+        'description': {
+            'type': str,
+            'required': True
+        },
+        'gdrive_link': {
+            'type': str,
+            'required': True
+        },
+        'shipping_terms': {
+            'type': str,
+            'required': True
+        },
+        'cost_model': {
+            'type': dict,
+            'required': True,
+            'whitelist': {
+                'wastepicker_sell_price': {
+                    'type': float,
+                    'required': True
+                },
+                'wastepicker_purchase_price': {
+                    'type': float,
+                    'required': True
+                },
+                'wholesaler_sell_price': {
+                    'type': float,
+                    'required': True
+                },
+                'price_buoyancy': {
+                    'type': float,
+                    'required': True
+                },
+                'wholesaler_exworks_price': {
+                    'type': float,
+                    'required': True
+                },
+                'wholesaler_shipping_price': {
+                    'type': float,
+                    'required': True
+                },
+                'wholesaler_delivered_price': {
+                    'type': float,
+                    'required': True
+                },
+                'pfc_transaction_fee': {
+                    'type': float,
+                    'required': True
+                },
+            }
+        },
+    }
+
 
 class Transaction(BaseMixin, db.Model):
     __tablename__ = 'transaction'
