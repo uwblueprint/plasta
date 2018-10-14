7cfa7d5df2e9cb9fe98fc03e72d643fe85e906c1
Clayton Halim <claytonhalim3@gmail.com>
2018-10-14 14:39:08 -0400

    Add small documentation

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 
 
 def create_app():
+    '''Called implicity by flask through flask run'''
+
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
