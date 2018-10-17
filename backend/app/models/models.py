5e7a237870564fe5e631bd2914aaeeeaf2b44463
Clayton Halim <claytonhalim3@gmail.com>
2018-10-16 22:54:51 -0400

    Separate metadata spec from models file

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import func
 from sqlalchemy.dialects.postgresql import JSONB
 
+from . import project_metadata
 from . import db
 from .mixins import BaseMixin
 
@@ -36,62 +37,7 @@ class Project(BaseMixin, db.Model):
     plastics = db.relationship('ProjectPlasticMap', back_populates='project')
     meta_data = db.Column(JSONB)
 
-    METADATA_WHITELIST = {
-        'brand_name': {
-            'type': str,
-            'required': True
-        },
-        'description': {
-            'type': str,
-            'required': True
-        },
-        'gdrive_link': {
-            'type': str,
-            'required': True
-        },
-        'shipping_terms': {
-            'type': str,
-            'required': True
-        },
-        'cost_model': {
-            'type': dict,
-            'required': True,
-            'whitelist': {
-                'wastepicker_sell_price': {
-                    'type': float,
-                    'required': True
-                },
-                'wastepicker_purchase_price': {
-                    'type': float,
-                    'required': True
-                },
-                'wholesaler_sell_price': {
-                    'type': float,
-                    'required': True
-                },
-                'price_buoyancy': {
-                    'type': float,
-                    'required': True
-                },
-                'wholesaler_exworks_price': {
-                    'type': float,
-                    'required': True
-                },
-                'wholesaler_shipping_price': {
-                    'type': float,
-                    'required': True
-                },
-                'wholesaler_delivered_price': {
-                    'type': float,
-                    'required': True
-                },
-                'pfc_transaction_fee': {
-                    'type': float,
-                    'required': True
-                },
-            }
-        },
-    }
+    METADATA_SPECIFICATION = project_metadata.SPECIFICATION 
 
 
 class Transaction(BaseMixin, db.Model):
