10a1e40cfe346b37214ab5817a15b99cdbee9abb
Nick Yang <nick.yang3721@gmail.com>
2018-10-20 19:09:53 +0000

    Modified setup script for server & added wsgi

--- backend/scripts/setup_postgres.sh
+++ backend/scripts/setup_postgres.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-createdb $(whoami) &> /dev/null # fixes postgres error
+user=$(whoami)
+
+echo "Creating database for ${user}"
+
+# sudo -u postgres createuser --interactive < $user < "y"
+# sudo -u postgres createdb $(whoami) &> /dev/null # fixes postgres error
 
 db_name='plasta'
 db_user="$db_name"
 db_pass='plasword'
 
-dropdb --if-exists "$db_name"
-psql -tAc "DROP ROLE IF EXISTS ${db_user};"
+sudo -u postgres dropdb --if-exists "$db_name"
+sudo -u postgres psql -tAc "DROP ROLE IF EXISTS ${db_user};"
 
-psql -tAc "CREATE USER $db_user WITH PASSWORD '${db_pass}';"
-createdb "$db_name"
+sudo -u postgres psql -tAc "CREATE USER $db_user WITH PASSWORD '${db_pass}';"
+sudo -u postgres createdb "$db_name"
 
 echo "Created user and database."
