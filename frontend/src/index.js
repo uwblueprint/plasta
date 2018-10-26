a8e55326333abf3cd989482113f44e14aa31ca00
Nick Yang <33774466+nkxy@users.noreply.github.com>
2018-10-25 23:28:59 -0400

    Revert "Switch to HashRouter"
    This reverts commit 5dbe979a97f80f7ccb38e45bfb693007df381c5b.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers';
 import LandingPage from './components/LandingPage';
