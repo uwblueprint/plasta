d7301c14929a37c3144740d414d2a2b8a69691dd
Xin Hao Zhang <xinhao.zhang98@gmail.com>
2018-11-08 21:00:28 -0500

    Reintroduce app component to fetch vendors on load

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,43 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers';
-import LandingPage from './components/LandingPage';
-import LoginPage from './components/LoginContainer';
-import NewProject from './components/PFCAdmin/NewProject';
-import ProjectPage from './components/PFCAdmin/ProjectPage';
-import AdminDashboard from './components/PFCAdmin/AdminDashboard';
-import AdminStakeholder from './components/PFCAdmin/AdminStakeholder';
-import AdminNewStakeholder from './components/PFCAdmin/AdminNewStakeholder';
-import CreateWastePicker from './components/DWCC/CreateWastePicker';
-import CreateExternalDWCC from './components/DWCC/CreateExternalDWCC';
-import './common.css';
-import DwccTransactionHistory from './components/DwccTransactionHistory';
-import DWCCBuyTransaction from './components/DWCC/DWCCBuyTransaction';
-import DWCCSellTransaction from './components/DWCC/DWCCSellTransaction';
+import App from './App';
 
 const store = createStore(rootReducer);
 
 render(
   <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route path="/dwcc/transactions/sell" exact component={DWCCSellTransaction} />
-        <Route path="/dwcc/transactions/buy" exact component={DWCCBuyTransaction} />
-        <Route path="/" exact component={LoginPage} />
-        <Route path="/landing" component={LandingPage} />
-        <Route path="/admindash" component={AdminDashboard} />
-        <Route path="/projects/new" component={NewProject} />
-        <Route path="/projects/:projectId" component={ProjectPage} />
-        <Route path="/admin/stakeholders/new" component={AdminNewStakeholder} />
-        <Route path="/admin/stakeholders" component={AdminStakeholder} />
-        <Route path="/dwcc/wastepickers/new" component={CreateWastePicker} />
-        <Route path="/dwcc/external-dwcc/new" component={CreateExternalDWCC} />
-        <Route path="/dwcc/transaction-history" component={DwccTransactionHistory} />
-      </Switch>
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
