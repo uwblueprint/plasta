4bad2d2587fecd59f2e7dcde2c88d21062fb2518
imruahmed <im.ahmed97@gmail.com>
2018-11-11 13:15:50 -0500

    Add empty list vendor endpoint

--- backend/app/routes/vendor_routes.py
+++ backend/app/routes/vendor_routes.py
@@ -6,6 +6,11 @@ from .route_utils import success
 blueprint = Blueprint('vendor', __name__, url_prefix='/vendors')
 
 
+@blueprint.route('/', methods=['GET'])
+def get_all_vendors():
+    return get_vendors([])
+
+
 @blueprint.route('/<list:vendor_types>', methods=['GET'])
 def get_vendors(vendor_types):
     vendors = db_client.get_vendors(vendor_types)
