599ab561d96b36eb5508d21eeb11683e621393ed
Nick Yang <nick.yang3721@gmail.com>
2018-11-22 19:47:25 -0500

    :wrench: Configure Redux DevTools extension

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,10 @@ import rootReducer from './reducers';
 import App from './App';
 import { CookiesProvider } from 'react-cookie';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 render(
   <CookiesProvider>
