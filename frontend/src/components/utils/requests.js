7948988025e39426d4761cee012e89aa1dcbb6ad
mmqw <mary.mqwang@gmail.com>
2018-11-25 16:26:51 -0500

    Redirecting user to correct page based on user type + moving DWCC transaction to DWCC folder + fix for currentUser redux store

--- frontend/src/components/utils/requests.js
+++ frontend/src/components/utils/requests.js
@@ -1,9 +1,18 @@
-export function post(path, payload) {
+function getAuthorizationHeader(cookies) {
+  return cookies
+    ? {
+        Authorization: `Bearer ${cookies.get('access_token')}`,
+      }
+    : {};
+}
+
+export function post(path, payload, cookies) {
   const url = process.env.REACT_APP_API_URL + path;
   const data = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      ...getAuthorizationHeader(cookies),
     },
     body: JSON.stringify(payload),
   };
@@ -18,6 +27,7 @@ export function post(path, payload) {
     });
 }
 
+<<<<<<< HEAD
 // TODO : accept multiple images
 export function postMultiType(path, fields) {
   const url = process.env.REACT_APP_API_URL + path;
@@ -32,13 +42,16 @@ export function postMultiType(path, fields) {
 }
 
 export function get(path, payload) {
+=======
+export function get(path, cookies) {
+>>>>>>> Redirecting user to correct page based on user type + moving DWCC transaction to DWCC folder + fix for currentUser redux store
   const url = process.env.REACT_APP_API_URL + path;
   const data = {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
+      ...getAuthorizationHeader(cookies),
     },
-    body: JSON.stringify(payload),
   };
   return fetch(url, data)
     .then(response => {
