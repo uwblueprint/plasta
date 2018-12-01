22f1635e38e593b2b5e23a994da1b463578781ac
imruahmed <im.ahmed97@gmail.com>
2018-12-01 15:00:52 -0500

    Require jwt tokens for all endpoints

--- backend/app/routes/project_routes.py
+++ backend/app/routes/project_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from flask_jwt_extended import jwt_required
 
 from . import db_client
 from .utils.route_utils import success
@@ -7,6 +8,7 @@ blueprint = Blueprint('project', __name__, url_prefix='/projects')
 
 
 @blueprint.route('/', methods=['GET'])
+@jwt_required
 def get_projects():
     projects = db_client.get_projects()
     return success(data=[
@@ -15,12 +17,14 @@ def get_projects():
 
 
 @blueprint.route('/', methods=['POST'])
+@jwt_required
 def create_project():
     project = db_client.create_project(request.json)
     return success(data=project.to_dict(include_relationships=True))
 
 
 @blueprint.route('/<int:project_id>/transactions', methods=['GET'])
+@jwt_required
 def get_project_transactions(project_id):
     transactions = db_client.get_project_transactions(project_id)
     return success(data=[
