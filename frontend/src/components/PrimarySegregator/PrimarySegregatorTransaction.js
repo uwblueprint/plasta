474d9c60afef476f35b1c9b08b4a2b62e387bd4a
Xin Hao Zhang <xinhao.zhang98@gmail.com>
2019-01-17 20:40:25 -0500

    presentation comp refactor

--- frontend/src/components/PrimarySegregator/PrimarySegregatorTransaction.js
+++ frontend/src/components/PrimarySegregator/PrimarySegregatorTransaction.js
@@ -1,124 +1,96 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import FormSection from '../input-components/FormSection';
 import SearchSelect from '../input-components/SearchSelect';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
-import 'react-day-picker/lib/style.css';
 import TextInput from '../input-components/TextInput';
-import { withRouter } from 'react-router-dom';
 import CreateStakeholderModal from './CreateStakeholderModal';
 import OnSubmitButton from '../common/OnSubmitButton';
 import { transactionTypes } from './HOCPrimarySegregatorTransaction';
 import { ruleTypes } from '../utils/form';
+import 'react-day-picker/lib/style.css';
 import './../FormPage.css';
 
-class PrimarySegregatorTransaction extends Component {
-  constructor(props) {
-    super(props);
-    this.buyStakeholderSelect = this.buyStakeholderSelect.bind(this);
-    this.sellStakeholderSelect = this.sellStakeholderSelect.bind(this);
-  }
-  buyStakeholderSelect() {
-    return (
-      <React.Fragment>
+const PrimarySegregatorTransaction = props => {
+  const pageTitle = props.transactionType === transactionTypes.BUY ? 'From *' : 'To *';
+  return (
+    <div className="page-wrapper" id="transactions-wrapper">
+      <h1>{props.title}</h1>
+      <p className="required-field-notif">
+        All fields marked with <b>*</b> are required.
+      </p>
+
+      {props.transactionType === transactionTypes.BUY && (
+        <CreateStakeholderModal show={props.showModal} handleClose={props.hideModal} />
+      )}
+
+      <FormSection title={pageTitle}>
         <SearchSelect
-          createable
+          createable={props.transactionType === transactionTypes.BUY}
           field="stakeholderName"
-          value={this.props.stakeholderName}
-          options={this.props.stakeholderOptions}
-          onChange={this.props.onFieldChange}
-          onNewOptionClick={this.props.handleNewStakeholder}
+          value={props.stakeholderName}
+          options={props.stakeholderOptions}
+          onChange={props.onFieldChange}
+          onNewOptionClick={props.handleNewStakeholder}
           promptTextCreator={search => `${search} not found, create a new stakeholder`}
-          onValidation={this.props.onValidation}
+          onValidation={props.onValidation}
           rules={[ruleTypes.FIELD_REQUIRED]}
-          showErrors={this.props.submitAttempted}
+          showErrors={props.submitAttempted}
         />
-        <CreateStakeholderModal show={this.props.showModal} handleClose={this.props.hideModal} />
-      </React.Fragment>
-    );
-  }
+      </FormSection>
 
-  sellStakeholderSelect() {
-    return (
-      <SearchSelect
-        field="stakeholderName"
-        value={this.props.stakeholderName}
-        options={this.props.stakeholderOptions}
-        onChange={this.props.onFieldChange}
-        onValidation={this.props.onValidation}
-        rules={[ruleTypes.FIELD_REQUIRED]}
-        showErrors={this.props.submitAttempted}
-      />
-    );
-  }
+      <FormSection className="formsection" title="Plastic Type *">
+        <SearchSelect
+          field="plasticType"
+          value={props.plasticType}
+          options={staticPlasticTypes}
+          onChange={props.onFieldChange}
+          onValidation={props.onValidation}
+          rules={[ruleTypes.FIELD_REQUIRED]}
+          showErrors={props.submitAttempted}
+        />
+      </FormSection>
 
-  render() {
-    return (
-      <div className="page-wrapper" id="transactions-wrapper">
-        <h1>{this.props.title}</h1>
-        <p className="required-field-notif">
-          All fields marked with <b>*</b> are required.
-        </p>
-        <FormSection
-          className="formsection"
-          title={`${this.props.transactionType === transactionTypes.BUY ? `From` : `To`} *`}
-        >
-          {this.props.transactionType === transactionTypes.BUY
-            ? this.buyStakeholderSelect()
-            : this.sellStakeholderSelect()}
-        </FormSection>
+      <FormSection className="formsection" title="Amount *">
+        <TextInput
+          id="unitPrice"
+          className="amount-input half-width inline margin-right-20"
+          field="unitPrice"
+          rightlabel=" ₹/Kg"
+          value={props.unitPrice}
+          placeholder={'0.00'}
+          type="number"
+          onChange={props.onFieldChange}
+          onValidation={props.onValidation}
+          rules={[ruleTypes.FIELD_REQUIRED]}
+          showErrors={props.submitAttempted}
+        />
+        <TextInput
+          id="weight"
+          className="amount-input half-width inline"
+          field="weight"
+          rightlabel=" Kg"
+          type="number"
+          value={props.weight}
+          placeholder={'0'}
+          onChange={props.onFieldChange}
+          onValidation={props.onValidation}
+          rules={[ruleTypes.FIELD_REQUIRED]}
+          showErrors={props.submitAttempted}
+        />
+      </FormSection>
 
-        <FormSection className="formsection" title="Plastic Type *">
-          <SearchSelect
-            field="plasticType"
-            value={this.props.plasticType}
-            options={staticPlasticTypes}
-            onChange={this.props.onFieldChange}
-            onValidation={this.props.onValidation}
-            rules={[ruleTypes.FIELD_REQUIRED]}
-            showErrors={this.props.submitAttempted}
-          />
-        </FormSection>
-        <FormSection className="formsection" title="Amount *">
-          <TextInput
-            id="unitPrice"
-            className="amount-input half-width inline margin-right-20"
-            field="unitPrice"
-            rightlabel=" ₹/Kg"
-            value={this.props.unitPrice}
-            placeholder={'0.00'}
-            type="number"
-            onChange={this.props.onFieldChange}
-            onValidation={this.props.onValidation}
-            rules={[ruleTypes.FIELD_REQUIRED]}
-            showErrors={this.props.submitAttempted}
-          />
-          <TextInput
-            id="weight"
-            className="amount-input half-width inline"
-            field="weight"
-            rightlabel=" Kg"
-            type="number"
-            value={this.props.weight}
-            placeholder={'0'}
-            onChange={this.props.onFieldChange}
-            onValidation={this.props.onValidation}
-            rules={[ruleTypes.FIELD_REQUIRED]}
-            showErrors={this.props.submitAttempted}
-          />
-        </FormSection>
-        <FormSection className="formsection" title="Date">
-          <DayPickerInput
-            className="date-input-field"
-            placeholder="YYYY-MM-DD"
-            onDayChange={day => this.props.handleDayChange('transactionDate', day)}
-          />
-        </FormSection>
-        <OnSubmitButton nextPath="/ps/transaction-history" onClick={this.props.onSubmit} />
-      </div>
-    );
-  }
-}
+      <FormSection className="formsection" title="Date">
+        <DayPickerInput
+          className="date-input-field"
+          placeholder="YYYY-MM-DD"
+          onDayChange={day => props.handleDayChange('transactionDate', day)}
+        />
+      </FormSection>
+      <OnSubmitButton nextPath="/ps/transaction-history" onClick={props.onSubmit} />
+    </div>
+  );
+};
 
 const staticPlasticTypes = [{ label: 'Green PET', value: 'green_pet' }];
 
