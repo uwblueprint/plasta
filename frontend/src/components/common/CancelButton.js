ce4460c27e67e8751cabe3b0000f37e87a731451
fakesquid <zjzhang1@gmail.com>
2019-03-14 19:05:55 -0400

    fixed Cancel Button

--- frontend/src/components/common/CancelButton.js
+++ frontend/src/components/common/CancelButton.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './CancelButton.css';
 import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class CancelButton extends Component {
   render() {
     return (
-      <button className="btn-dark CancelButton" onClick={this.props.history.goBack}>
-        <i className="fas fa-times" style={{ marginRight: '1em' }} />
-        Cancel
-      </button>
+      <Link to="/ps/transaction-history">
+        <button className="btn-dark CancelButton">
+          <i className="fas fa-times" style={{ marginRight: '1em' }} />
+          Cancel
+        </button>
+      </Link>
     );
   }
 }
