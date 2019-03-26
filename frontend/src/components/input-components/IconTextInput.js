6d2a24993748389d320e178e255b125a0e67f733
Sherry Li <sherryhli@users.noreply.github.com>
2019-03-25 20:14:58 -0400

    Fix icon sizings

--- frontend/src/components/input-components/IconTextInput.js
+++ frontend/src/components/input-components/IconTextInput.js
@@ -26,11 +26,11 @@ const IconTextInput = props => {
   const { id, classes, errors, showErrors, iconimage, ...rest } = props;
   const isErrorState = showErrors && props.errors.length > 0;
   return (
-    <Grid container spacing={16} alignItems="center" wrap="nowrap" style={{ marginBottom: 5 }}>
+    <Grid container spacing={8} alignItems="center" wrap="nowrap" style={{ marginBottom: 5 }}>
       <Grid item xs={1}>
         {props.iconimage}
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={11}>
         <TextField
           id={id}
           InputProps={{ className: classes.input, required: true }}
