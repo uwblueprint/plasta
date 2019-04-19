974122ce41590144c1cc2d64efd087356daf1073
Nisam <nsk@phases.dk>
2019-04-19 11:23:29 +0530

    removed s3 image upload

--- backend/app/routes/db_client.py
+++ backend/app/routes/db_client.py
@@ -57,10 +57,10 @@ def get_user(email):
 
 
 def create_vendor(data, current_user=None, files=None):
-    from . import s3_client
-    if files is not None and 'picture' in files:
-        image_link = s3_client.upload_image(files['picture'])
-        data['image_link'] = image_link
+    # from . import s3_client
+    # if files is not None and 'picture' in files:        
+    #     image_link = s3_client.upload_image(files['picture'])
+    #     data['image_link'] = image_link
 
     vendor = Vendor.create(**data)
 
